641642f3f4913374568d8cdbb10b3a9d05e5d9f2
Zach Parker <evergreenzach@gmail.com>
2023-08-30 15:09:47 -0600

    updated nested dict assign

--- Assignments/Nesteddictionaries_py/main.py
+++ Assignments/Nesteddictionaries_py/main.py
@@ -39,11 +39,11 @@ students = [
     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
     {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-def iterateDictionary(students):
-    for key in students:
-        print('-',key)
-        
-iterateDictionary(students)
+# def iterateDictionary(students):
+    
+#     for key in students:
+#         print(key)
+# iterateDictionary(students)
 """iterateDictionary(students) 
 should output: (it's okay if each key-value pair ends up on 2 separate lines;
 bonus to get them to appear exactly as below!)
