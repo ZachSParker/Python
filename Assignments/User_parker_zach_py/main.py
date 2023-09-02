814b5e30a26520206b90521399755eddcbba3858
Zach Parker <evergreenzach@gmail.com>
2023-09-02 08:31:47 -0600

    chained methods together inside User class

--- Assignments/User_parker_zach_py/main.py
+++ Assignments/User_parker_zach_py/main.py
@@ -14,17 +14,17 @@ class User:
         else:
             self.is_rewards_member = True
             self.gold_card_pts = 200
+            return self
     def spend_points(self,amount):
         if self.gold_card_pts > amount:
             self.gold_card_pts -= amount
             self.monthly_pts_usage += amount
         else:
             print(f"{self.full_name} needs to watch their spending")
-        print(f"Thank you for your patronage {self.first_name} {self.last_name}")
+        print(f"Thank you for your patronage {self.full_name}")
         print(f"Your new points balance is: {self.gold_card_pts}")
         print(f"Your monthly expenditure of points is: {self.monthly_pts_usage}")
-        
-        
+        return self
     def display_u_Info(self):
         print(f"Name:{self.full_name}")
         print(f"Email Address:{self.email}")
@@ -35,16 +35,15 @@ class User:
             print("X Rewards card not activated")
         print(f"Current Gold Points:{self.gold_card_pts}")
         print(f"Monthly Points used:{self.monthly_pts_usage}")
+        return self
 ada_User = User('Ada','Wong','wong@gmail.com',28)
 mark_User =User('Mark','Aurelius','mAurel@yahoo.com',37)
 brian_User =User('Brian','Badluck','forevercursed@hotmail.com',42)
-User.enroll_rewards(ada_User)
-User.enroll_rewards(ada_User)
-User.enroll_rewards(mark_User)
-User.enroll_rewards(brian_User)
-User.spend_points(ada_User,50)
-User.spend_points(mark_User,80)
-User.spend_points(brian_User,40)
-User.display_u_Info(ada_User)
-User.display_u_Info(mark_User)
-User.display_u_Info(brian_User)
+
+ada_User.enroll_rewards().spend_points(50).display_u_Info()
+mark_User.enroll_rewards().spend_points(80).display_u_Info()
+brian_User.enroll_rewards().spend_points(40).display_u_Info()
+
+
+
+
