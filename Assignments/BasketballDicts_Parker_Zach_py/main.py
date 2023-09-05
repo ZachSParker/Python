668f17e6e43a49a731bca6f6b5634a47b440045f
Zach Parker <evergreenzach@gmail.com>
2023-09-04 20:24:14 -0600

    updating player dict to initialize objs

--- Assignments/BasketballDicts_Parker_Zach_py/main.py
+++ Assignments/BasketballDicts_Parker_Zach_py/main.py
@@ -1,21 +1,18 @@
 class Player:
-    def __init__(self, name, age, position, team):
+    def __init__(self,name,age,position,team):
         self.name = name
         self.age = age
         self.position = position
         self.team = team
-# players = [
-#     {"name":"Kevin Durant","age":34,"position":"small forward","team":"Brooklyn Nets"},
-#     {"name":"Jason Tatum","age":24,"position":"small forward","team":"Boston Celtics"},
-#     {"name":"Kyrie Irving","age":32,"position":"Point Guard","team":"Brooklyn Nets"}
-#     ]
-# for items in players:
-#     playerInfo = Player(
-#         items["name"],
-#         items["age"],
-#         items["position"],
-#         items["team"])
-    
+player = [
+    {"name":"Kevin Durant","age":34,"position":"small forward","team":"Brooklyn Nets"},
+    {"name":"Jason Tatum","age":24,"position":"small forward","team":"Boston Celtics"},
+    {"name":"Kyrie Irving","age":32,"position":"Point Guard","team":"Brooklyn Nets"}
+    ]
+
+
+
+
 
 
 
